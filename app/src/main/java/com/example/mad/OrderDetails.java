b8cf6cbb4f56a989c83d050ee88939f10333c059
Binuri-Karunathilake <it20023300@my.sliit.lk>
2021-09-02 00:40:27 +0530

    Initial commit(updated)

--- app/src/main/java/com/example/mad/OrderDetails.java
+++ app/src/main/java/com/example/mad/OrderDetails.java
@@ -10,6 +10,6 @@ public class OrderDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_details);
-
+        getSupportActionBar().hide();
     }
 }
