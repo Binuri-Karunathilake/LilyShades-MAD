8713a564395e4d7327e773ae388ebf2d18f95e20
Nuwanga-Wijamuni <it19091112@my.sliit.lk>
2021-09-22 20:39:11 +0530

    This is My First Commit

---
+++ app/src/main/java/com/example/foodorder/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.foodorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignin;
+    TextView txtslogen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
