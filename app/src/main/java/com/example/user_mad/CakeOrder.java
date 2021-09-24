fed08680babcaf5609529a865cdb43a2a94c59fe
RashaniTharushika <rashanitharushika99@gmail.com>
2021-09-24 12:41:21 +0530

    initial commit

---
+++ app/src/main/java/com/example/user_mad/CakeOrder.java
@@ -0,0 +1,15 @@
+package com.example.user_mad;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CakeOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cake_order);
+    }
+}
